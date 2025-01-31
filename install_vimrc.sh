84a6b13d64fef4091dfe4c630a231105c1787e69
Emil <e.motroen@gmail.com>
2025-01-31 20:43:13 +0100

    Functioning script to move .vimrc file

---
+++ install_vimrc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+command -v vim >/dev/null 2>&1 ||
+    { echo >&2 "Vim is required, but it's not installed. Aborting."; exit 1; }
+
+# Check if a .vimrc file exists
+file=".vimrc"
+if [[ -f "$HOME/$file" && -s "$HOME/$file" ]]; then
+    echo Non-empty $file found in "$HOME" -- Creating a backup of it.
+    mv $HOME/$file $HOME/.vimrc_backup
+fi
+
+# Move .vimrc file from this directory to the correct directory
+mv $file $HOME/$file
+echo Moved .vimrc file from this directory to "$HOME"
