d81d802721ec385f19e7b23e68edcc475cb6c921
Emil <e.motroen@gmail.com>
2025-01-31 22:02:10 +0100

    Create symlink to .vimrc

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -6,10 +6,16 @@ command -v vim >/dev/null 2>&1 ||
 # Check if a .vimrc file exists
 file=".vimrc"
 if [[ -f "$HOME/$file" && -s "$HOME/$file" ]]; then
-    echo Non-empty $file found in "$HOME" -- Creating a backup of it.
+    echo Non-empty $file found in $HOME -- Creating a backup of it.
     mv $HOME/$file $HOME/.vimrc_backup
 fi
 
 # Move .vimrc file from this directory to the correct directory
 mv $file $HOME/$file
-echo Moved .vimrc file from this directory to "$HOME"
+echo Moved .vimrc file from this directory to $HOME.
+
+# Create a symlink so future changes can be easily added
+ln -s $HOME/$file $file
+ls -l $file
+
+echo Successfully created symlink.
